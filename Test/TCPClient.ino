643e1914ec9d9ae1fd7c29755cbb99886decdc04
Ib Leminen Mohr Nielsen <144134079+ibxx0402@users.noreply.github.com>
2024-03-13 08:36:35 +0100

    Added sleep functionality
    Instead of running in a constant loop, it will sleep in intervals and then power on instead.

--- Test/TCPClient.ino
+++ Test/TCPClient.ino
@@ -8,13 +8,12 @@ dht11 DHT11;
 
 const char* ssid = "Linksys00339";//put your wifi ssid here
 const char* password = "GoBoat33";//put your wifi password here.
-const char* serverAddress = "192.168.1.140"; // TCP Server's IP ADDRESS Raspberry Pi IP: 192.168.1.10
+const char* serverAddress = "192.168.1.10"; // TCP Server's IP ADDRESS Raspberry Pi IP: 192.168.1.10
 const int serverPort = 8888; //server's port 
-int RandNumber;
-int TempTest = 25;
-String UID = "ESP32";
-String Payload;
 
+int RandNumber; //empty parameter to use with Random()
+String UID = "ESP32"; //uniqe id, so its possible to sort them, ## current name is a placeholder
+String Payload; //empty string parameter, later used for joining strings.
 
 WiFiClient TCPclient;
 void setup_wifi() { //Connect to wifi. While loop breaks when connected.
@@ -28,24 +27,26 @@ void setup_TCP() { //Connect to TCP server. While loop breaks when connected.
 }
 
 void setup() {
+  esp_sleep_enable_timer_wakeup(60000000*5); // 60000000==1 min 
   Serial.begin(115200); //Makes it possible to print, remember to set IDE to 115200 baud. 
   setup_wifi(); // Runs setup_wifi function. 
   setup_TCP(); // Runs setup_TCP function.
   Serial.println("connected");
-}
-
-void loop() { //Loop function is mandatory even if it is not used.
-  delay(2000); // Time specified in milliseconds.
-  setup_TCP(); // Runs setup_TCP function.
-
-  int chk = DHT11.read(DHT11PIN); //Reads information from pin.
+  
+  int chk = DHT11.read(DHT11PIN);
   char Temp = DHT11.temperature;
 
-  RandNumber = random(100); // Random number from 0 to 100.
-  Payload = UID +","+ String(RandNumber) +","+ String(byte(Temp)); // combines UID, RandNumber and Temperture.
-  
-  TCPclient.write(Payload.c_str()); // sends the Payload
-  Serial.print("Payload sent - "); // prints the Payload
-  Serial.println(Payload.c_str()); 
+  RandNumber = random(100);
+  Payload = UID +","+ String(RandNumber) +","+ String(byte(Temp));
 
+  TCPclient.write(Payload.c_str());
+  Serial.print("Payload sent - ");
+
+  Serial.println(Payload.c_str());
+  delay(80);
+
+  esp_deep_sleep_start();
+}
+
+void loop() { //Loop function is needed even if it is not used.
 }

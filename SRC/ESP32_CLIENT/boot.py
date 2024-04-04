7ae09270a9d429b8eadbbb46a7505c5df4790809
Ib Leminen Mohr Nielsen <144134079+ibxx0402@users.noreply.github.com>
2024-04-04 14:12:38 +0200

    changed function name in boot.py
    changed function name and moved some docstrings

--- SRC/ESP32_CLIENT/boot.py
+++ SRC/ESP32_CLIENT/boot.py
@@ -7,9 +7,10 @@ Connects to WifI and sends a simple message.
 """
 
 def connect_to_wifi(): #forbindelse til WiFi.
-"""
-Function makes it possible to connect ESP32 to WiFi.
-"""
+    """
+    Function makes it possible to connect ESP32 to WiFi.
+    """
+
     ssid = "Linksys00339" #Network's name.
     key="GoBoat33" #Network's passcode.
     wlan = network.WLAN(network.STA_IF)
@@ -20,11 +21,12 @@ Function makes it possible to connect ESP32 to WiFi.
             pass
 
 def client_to_server():
-"""
-Connects the client to the server using TCP (socket)
-Raspberry Pi IP_Adress = 192.168.1.10
-Make sure to be on same network as server and not eduroam
-"""
+    """
+    Connects the client to the server using TCP (socket)
+    Raspberry Pi IP_Adress = 192.168.1.10
+    Make sure to be on same network as server and not eduroam
+    """
+    
     server_ip = "192.168.1.140" #The server's IP adress 
     server_port = 8888 
     buffer_size = 1024 
@@ -34,6 +36,7 @@ Make sure to be on same network as server and not eduroam
     sock.connect((server_ip, server_port)) #Establishes connection to server.
     sock.send(bytes(payload,'utf-8')) #Sends the payload in bytes formatted in utd-8
 
+
 #running the functions.
-connect_to_server()
+connect_to_wifi()
 client_to_server()

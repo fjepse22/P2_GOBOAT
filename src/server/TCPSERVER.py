bd79e0c3c13f794a9a4aa99f93f5f332caf0711a
Ib Leminen Mohr Nielsen <144134079+ibxx0402@users.noreply.github.com>
2024-04-23 11:55:59 +0200

    added xml parser

--- src/server/TCPSERVER.py
+++ src/server/TCPSERVER.py
@@ -1,6 +1,6 @@
 import socket
 import selectors
-import time 
+#from xml_parser import XmlParser
 
 HOST = ''           # Symbolic name meaning all available interfaces
 PORT = 8888         # Arbitrary non-privileged port
@@ -8,21 +8,22 @@ CONN_COUNTER = 0    # Counter for connections
 BUFFER_SIZE = 1024  # Receive Buffer size (power of 2)
 sel = selectors.DefaultSelector()
 
+print('* TCP Server listening for incoming connections in port {}'.format(PORT))
+
 def accept(sock, mask):
     conn, addr = sock.accept() # Should be ready
+    #print('accepted', conn, 'from', addr)
     conn.setblocking(False)
     sel.register(conn, selectors.EVENT_READ, read)
     
 def read(conn, mask):
-    data = conn.recv(1000) # Should be ready
-    local_time = time.ctime(time.time())
-  
-    with open ('log.txt', 'a') as file: #creates log.txt and appends the data, time.
-        file.write(str(data, 'utf-8')+" "+ local_time +" "+ ('\n'))
-      
+    data = conn.recv(3000) # Should be ready
+    #xml_parser= XmlParser(xml_path=str(data, 'UTF-8'),xsd_path="sch_status_data.xsd",xml_from_file=False)
+    #xml_parser.get_all_data()
+    #print(xml_parser)
+
     sel.unregister(conn)
     conn.close()
-
 sock = socket.socket()
 sock.bind((HOST, PORT))
 sock.listen(100)

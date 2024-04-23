4ad3328160ca6b248f38407af787b04d1eaf6d00
fjepse22 <144028671+fjepse22@users.noreply.github.com>
2024-04-23 11:14:12 +0200

    Update packet_controller.py
    Made it possible to check an xml-file if it is a string variable in python instead of only from a file.

--- src/xml_parser/packet_controller.py
+++ src/xml_parser/packet_controller.py
@@ -1,11 +1,19 @@
+#Version 0.2 | Encoding UTF-8
+#Created 22-04-2024
+#Created by: Ib Leminen Mohr Nielsen
+#Modified by: Frederik B. B. Jepsen, Ib Leminen Mohr Nielsen
+#Last modified 23-04-2024
+
 from lxml import etree
-
-def validate(xml_path: str, xsd_path: str) -> bool:
-
+        
+def validate(xsd_path: str, xml_path: str) -> bool:
     xmlschema_doc = etree.parse(xsd_path)
     xmlschema = etree.XMLSchema(xmlschema_doc)
-
-    xml_doc = etree.parse(xml_path)
+    try:
+            xml_doc = etree.parse(xml_path)
+    except:
+        # if xml-path is not a file, it will read the xml-file as a string instead
+        xml_doc= etree.fromstring(xml_path)
     result = xmlschema.validate(xml_doc)
 
     return result

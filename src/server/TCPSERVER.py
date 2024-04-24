5b27a5fb31291c2cbf128ffb5af57385f736b5fd
Ib Leminen Mohr Nielsen <144134079+ibxx0402@users.noreply.github.com>
2024-04-24 15:54:41 +0200

    Added support for win and rasp

--- src/server/TCPSERVER.py
+++ src/server/TCPSERVER.py
@@ -1,4 +1,4 @@
-#Version 0.21 | Encoding UTF-8
+#Version 0.22 | Encoding UTF-8
 #Created 23-04-2024
 #Created by: Ib Leminen Mohr Nielsen
 #Modified by: Frederik B. B. Jepsen, Ib Leminen Mohr Nielsen
@@ -26,24 +26,26 @@ def accept(sock, mask):
     #print('accepted', conn, 'from', addr)
     conn.setblocking(False)
     sel.register(conn, selectors.EVENT_READ, read)
-    
+
 def read(conn, mask):
     data = b''
-    while True:
-        data += conn.recv(BUFFER_SIZE)
-        if data[-3:] == b'EOF':
-            break
-    xml_parser= XmlParser(xsd_path="sch_status_data.xsd", xml_path=str(data[:-3], 'UTF-8'))
-    xml_parser.get_all_data()
-    print(xml_parser)
-    Goboat = sql.DatabaseConnection(user="testuser",password="testpassword",host="127.0.0.1")
-    input = xml_parser
-
-    Goboat.insert_boat_data(boat_ID=input.boat_id,Date=input.date,Lok_lat=input.lok_lat,Lok_long=input.lok_long,Battery_temperatures=input.temp_list,Watt_hour=input.watt_hour,Voltage_array=input.voltage_list)
+    while b"EOF" not in data: # runs while EOF(end of file) is not in the data, which is recieved last from the client. 
+        try:
+            data += conn.recv(BUFFER_SIZE) # used try except to avoid blocking error
+        except BlockingIOError:
+            continue
+    
     sel.unregister(conn)
     conn.close()
 
-    
+    xml_parser= XmlParser(xsd_path="sch_status_data.xsd", xml_path=str(data[:-3], 'UTF-8')) #inserets the xml data into the xml_parser from the client. 
+    xml_parser.get_all_data()
+    Goboat = sql.DatabaseConnection(user="testuser",password="testpassword",host="127.0.0.1") # establish connection to the database
+    input = xml_parser
+
+    Goboat.insert_boat_data(boat_ID=input.boat_id,Date=input.date,Lok_lat=input.lok_lat,Lok_long=input.lok_long,Battery_temperatures=input.temp_list,Watt_hour=input.watt_hour,Voltage_array=input.voltage_list)
+    #inserts the data into the database
+
 sock = socket.socket()
 sock.bind((HOST, PORT))
 sock.listen(100)

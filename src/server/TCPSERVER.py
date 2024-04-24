9400bcf41c504c78ad5a2f742d5ee4a302e448ed
Ib Leminen Mohr Nielsen <144134079+ibxx0402@users.noreply.github.com>
2024-04-24 09:38:28 +0200

    Now stops when client sends b'E0F'

--- src/server/TCPSERVER.py
+++ src/server/TCPSERVER.py
@@ -1,18 +1,17 @@
-#Version 0.2 | Encoding UTF-8
+#Version 0.21 | Encoding UTF-8
 #Created 23-04-2024
 #Created by: Ib Leminen Mohr Nielsen
 #Modified by: Frederik B. B. Jepsen, Ib Leminen Mohr Nielsen
-#Last modified 23-04-2024
+#Last modified 24-04-2024
 
 import socket
 import selectors
 from xml_parser import XmlParser
 import sql_insert_data as sql
 
-# This needs to be event based with a que system, in order to ensure that all data gets into the database
-# there is a bug where the xml-file is not always correctly recieved.
-
-
+"""
+This needs to be event based with a que system, in order to ensure that all data gets into the database
+"""
 
 HOST = ''           # Symbolic name meaning all available interfaces
 PORT = 8888         # Arbitrary non-privileged port
@@ -24,24 +23,27 @@ print('* TCP Server listening for incoming connections in port {}'.format(PORT))
 
 def accept(sock, mask):
     conn, addr = sock.accept() # Should be ready
-    print('accepted', conn, 'from', addr)
+    #print('accepted', conn, 'from', addr)
     conn.setblocking(False)
     sel.register(conn, selectors.EVENT_READ, read)
     
 def read(conn, mask):
-    data = conn.recv(5000) # Should be ready
-    xml_parser= XmlParser(xml_path=str(data, 'UTF-8'),xsd_path="sch_status_data.xsd",xml_from_file=False)
+    data = b''
+    while True:
+        data += conn.recv(BUFFER_SIZE)
+        if data[-3:] == b'EOF':
+            break
+    xml_parser= XmlParser(xsd_path="sch_status_data.xsd", xml_path=str(data[:-3], 'UTF-8'))
     xml_parser.get_all_data()
-    print(str(data,'UTF-8'))
-
-    Goboat = sql.DatabaseConnection(user="frederik",password="password",host="127.0.0.1")
+    print(xml_parser)
+    Goboat = sql.DatabaseConnection(user="testuser",password="testpassword",host="127.0.0.1")
     input = xml_parser
 
     Goboat.insert_boat_data(boat_ID=input.boat_id,Date=input.date,Lok_lat=input.lok_lat,Lok_long=input.lok_long,Battery_temperatures=input.temp_list,Watt_hour=input.watt_hour,Voltage_array=input.voltage_list)
-
-
     sel.unregister(conn)
     conn.close()
+
+    
 sock = socket.socket()
 sock.bind((HOST, PORT))
 sock.listen(100)

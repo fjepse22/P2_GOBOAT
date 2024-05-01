8630420be42551487cecb53cc24f444d8b61aeb6
Ib Leminen Mohr Nielsen <144134079+ibxx0402@users.noreply.github.com>
2024-05-01 11:58:32 +0200

    instead of running file it runs class
    - Added: Docstrings 
    - Added: Ability to change directory from thread_manager.py

--- src/thread_manager/thread_manager.py
+++ src/thread_manager/thread_manager.py
@@ -1,16 +1,43 @@
+#Version 0.10 | Encoding UTF-8
+#Created 26-04-2024
+#Created by: Ib Leminen Mohr Nielsen
+#Modified by: Frederik B. B. Jepsen, Ib Leminen Mohr Nielsen
+#Last modified 1-05-2024
+
 import threading
-import subprocess
-import sys 
+from TCPSERVER import SQL_socket
 
 class ThreadManager:
-    def run_script(self, script_name):
-        subprocess.run(["python", script_name])
+    """
+    The class ThreadManager is used to run the TCPSERVER on a thread.\n 
 
-    def run_thread(self, script_name):
-        thread = threading.Thread(target=self.run_script, args=(script_name,))
-        thread.start()
+    List of class methods:\n
+    - run_server(self, directory): Runs the server on a thread, uses directory to specify what folder it is running from.\n
+
+    """
+
+    def run_server(self, directory="/home/Gruppe250/test"):
+        """
+        Runs the server on a thread, uses directory to specify what folder it is running from.\n
+        \n
+        ------------
+        PARAMETERS\n
+        directory = The path of the file to check xml-integrety.\n
+    
+        self:\n
+        ------------
+        RETURNS\n
+        \n
+        Returns "None"\n
+        Return None\n
+        """
+
+        server=SQL_socket(user="testuser",password="testpassword", host="127.0.0.1", directory=directory)
+        print('* TCP Server listening for incoming connections in port {}'.format(server.PORT))
+        thread = threading.Thread(target=server.run) #Creates a thread, with the server as the target
+        thread.start() #Starts the server on a thread
 
 if __name__ == "__main__":
-    print("Thread manager is running.")
     thread_manager = ThreadManager()
-    thread_manager.run_thread(str("/home/Gruppe250/test/TCPSERVER.py",))
+    thread_manager.run_server() #if you want to change directory, you can add it as a parameter here ex. macOS run_server("/Users/ibleminen/Downloads/test/rasp") 
+

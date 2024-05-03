378fd99f939e53a21976a054f5cbe8144d05918d
fjepse22 <144028671+fjepse22@users.noreply.github.com>
2024-05-03 12:57:47 +0200

    Update sql_insert_data.py
    Updated this module, so it inserts to the new database instead

--- src/xml_parser/sql_insert_data.py
+++ src/xml_parser/sql_insert_data.py
@@ -1,5 +1,5 @@
 # See https://mariadb-corporation.github.io/mariadb-connector-python/usage.html for documentation about the mariadb module.
-# Version 1.22
+# Version 1.23
 # Writen by Frederik B. B. Jepsen
 # Created 13-04-2024
 # last modified: 3-05-2024
@@ -17,7 +17,7 @@ class DatabaseConnection:
 
     List of class methods:\n
     - __init__(self,user,password,host,port=3306,database='Goboat', directory=''): Initializes the class with the user, password, host, port, database and directory.\n
-    - insert_boat_data(self,boat_ID,Date,Lok_lat,Lok_long,Battery_temperature,watt,Voltage_array): Inserts the data into the Goboat database.\n
+    - insert_boat_data(self,boat_ID,date,lok_lat,lok_long,temperature,watt,voltage_array): Inserts the data into the Goboat database.\n
 
     """
   
@@ -32,22 +32,22 @@ class DatabaseConnection:
         self.port = port
         self.database = database
 
-    def insert_boat_data(self,boat_ID,Date,Lok_lat,Lok_long,Battery_temperatures,watt,Voltage_array):
+    def insert_boat_data(self,boat_ID,date,lok_lat,lok_long,temperatures,watt,voltage_array):
         """
         This method connects to the Goboat database and insert the data recieved from the xml data.\n
-        It will only insert the data, if the amount of batteries for the boat corresponding to the length of the Voltage_array.\n
-        It will insert data into the tables Data_Boat and Voltage.\n
+        It will only insert the data, if the amount of batteries for the boat corresponding to the length of the voltage_array.\n
+        It will insert data into the tables boat_log and Voltage.\n
         If somethings goes wrong no changes will be commited into the database.\n
         \n
         ------------
         PARAMETERS\n
         boat_ID: The ID of the boat\n
-        Date: The date and time of the data\n
-        Lok_lat: The latitude of the boat\n
-        Lok_long: The longitude of the boat\n
-        Battery_temperatures: The temperature of the batteries in a list\n
+        date: The date and time of the data\n
+        lok_lat: The latitude of the boat\n
+        lok_long: The longitude of the boat\n
+        temperatures: The temperature of the batteries in a list\n
         watt: The amount of watt hours\n
-        Voltage_array: The voltage of the batteries in a list\n
+        voltage_array: The voltage of the batteries in a list\n
         \n
         self:\n
         ------------
@@ -67,20 +67,20 @@ class DatabaseConnection:
 
 
         # The command to insert all data of the boat except the voltage of the battery.
-        insert_data = f"""INSERT INTO Goboat.Data_Boat (Boat_ID,Data_time,Lok_lat,Lok_long,watt)
-        VALUES ('{boat_ID}','{Date}','{Lok_lat}','{Lok_long}',{watt});"""
+        insert_data = f"""INSERT INTO goboatv2.boat_log (boat_ID,data_time,lok_lat,lok_long,watt)
+        VALUES ('{boat_ID}','{date}','{lok_lat}','{lok_long}',{watt});"""
         
 
         # this command is used to find the Data_ID in order to link the data about battery voltage to the rest of the boat data.
         # it is possible to extract this information before you commit the data, as the Data_ID get reserved for the duration.
         find_Data_ID = f"""SELECT Data_ID
-        From Goboat.Data_Boat
-        WHERE (Boat_ID='{boat_ID}') AND (Data_time='{Date}'); """
+        From goboatv2.boat_log
+        WHERE (boat_ID='{boat_ID}') AND (data_time='{date}'); """
 
 
-        find_batteries =f""" SELECT Battery_ID,Slot_number
-        FROM Goboat.Boats_batteries
-        WHERE (Boat_ID='{boat_ID}');"""
+        find_batteries =f""" SELECT bat_ID,slot_no
+        FROM goboatv2.boat_conf
+        WHERE (boat_ID='{boat_ID}');"""
         
 
         # fetch the batteri_id for each batteri in the boat.
@@ -98,7 +98,7 @@ class DatabaseConnection:
 
         # this check that the amount of batteries on the boat correnspont to the voltage_array data
         # if that is not the case, no data will be inserted into the Goboat database.
-        if len(batteries) == len(Voltage_array):
+        if len(batteries) == len(voltage_array):
             pass    
         else:
             # raise Exception(f'The number of batteries on boat {boat_ID} in the database does not correspond to the amount of batteries comming from the xml-file')
@@ -111,7 +111,7 @@ class DatabaseConnection:
             cursor.execute(insert_data)
                 
         except mariadb.Error as e:
-            self.logger.error(f"insert into table Data_boat failed: {e}" )
+            self.logger.error(f"insert into table boat_log failed: {e}" )
             cursor.close()
             connection.close()
             return
@@ -121,7 +121,7 @@ class DatabaseConnection:
             fetch = cursor.fetchall()
             Data_ID = fetch[0][0]
         except mariadb.Error as e:
-            self.logger.error(f'select statement failed could not find Data_ID {Data_ID} in table Data_boat {e}')
+            self.logger.error(f'select statement failed could not find Data_ID {Data_ID} in table boat_log {e}')
             cursor.close()
             connection.close()
             return
@@ -133,9 +133,9 @@ class DatabaseConnection:
         
         # Inserts the 
         try:
-            for i in range(0,len(Voltage_array)):
-                cursor.execute(f""" INSERT INTO Goboat.Voltage (Data_ID,Battery_ID,Battery_temperature,Battery_voltage)
-            VALUES ('{Data_ID}','{batteries[i][0]}','{Battery_temperatures[i]}','{Voltage_array[i]}');""")
+            for i in range(0,len(voltage_array)):
+                cursor.execute(f""" INSERT INTO goboatv2.battery_log (Data_ID,bat_ID,temperature,voltage)
+            VALUES ('{Data_ID}','{batteries[i][0]}','{temperatures[i]}','{voltage_array[i]}');""")
         except mariadb.Error as e:
             self.logger.error(f"failed to insert data to the voltage table {e}")
             cursor.close()
@@ -153,9 +153,9 @@ class DatabaseConnection:
 if __name__ == '__main__':
 
     # Testserver on Frederik's own computer.
-    Goboat = DatabaseConnection(user="frederik",password="password",host="127.0.0.1")
+    Goboat = DatabaseConnection(user="frederik",password="password",host="127.0.0.1",database='goboatv2',directory="C:/Users/frede\Desktop/sql_v2")
     data = xml_p.XmlParser()
     data.get_all_data()
 
-    Goboat.insert_boat_data(boat_ID=data.boat_id,Date=data.date,Lok_lat=data.lok_lat,Lok_long=data.lok_long,Battery_temperatures=data.temp_list,watt=data.watt,Voltage_array=data.voltage_list)
+    Goboat.insert_boat_data(boat_ID=data.boat_id,date=data.date,lok_lat=data.lok_lat,lok_long=data.lok_long,temperatures=data.temp_list,watt=data.watt,voltage_array=data.voltage_list)
 

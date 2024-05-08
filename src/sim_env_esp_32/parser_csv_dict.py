1d2737454d68e25c4ed2f55aaf3648103d8653df
Jesper Hammer <jhamme19@student.aau.dk>
2024-05-08 15:33:02 +0200

    Bulk upload of sim_env_esp_32 files

---
+++ src/sim_env_esp_32/parser_csv_dict.py
@@ -0,0 +1,110 @@
+# Version 1.00 | Encoding UFT-8
+# Created by: Jesper Hammer
+# Date: 02-05-2024
+
+from logger import Logger
+
+class CSVDictParser:
+    """
+    Parses .csv-files and returns dict with selected type\n
+    List of class methods:\n
+    - csv_dict_parser_float: parses csv-file to dict containing float variable(s)\n
+    - csv_dict_parser_str: parses csv-file to dict containing str variable(s)\n
+    """
+
+    def __init__(self, log_file) -> None:
+        """
+        Initialises the logger
+        \n
+        ------------
+        PARAMETERS\n
+        log_file:\n
+        Takes log file name of type STRING\n
+        File must be of type TXT\n
+        \n
+        ------------
+        RETURNS\n
+        Returns None\n
+        \n
+        """
+
+        self.log = Logger(__name__, log_file)
+
+    def csv_dict_parser_float(self, in_file: str) -> dict:
+
+        """
+        Parses csv-file to list containing float variable(s)\n
+        \n
+        ------------
+        PARAMETERS\n
+        in_file:\n
+        Takes input file name of type STRING\n
+        File must be of type CSV\n
+        \n
+        ------------
+        RETURNS\n
+        Returns a dict of values\n
+        Return is of type DICT\n
+        List elements is of type FLOAT\n
+        \n
+        """
+
+        return_file = {}
+
+        #OPEN FILE AND CREATE READER OBJ
+        reader = []
+        delim = ','
+
+        try:
+            with open(in_file,'r') as file:
+                for line in file:
+                    reader.append(line.rstrip('\n').rstrip('\r').split(delim))
+
+                self.log.debug(f"opened {in_file}")
+                #PARSE DATA FROM CSV TO DICT
+                for line in reader:
+                    add_data = {float(line[0]) : float(line[1])}
+                    return_file.update(add_data)
+            return return_file
+        except Exception as e:
+            self.log.critical (f"An error occured while parsing CSV: {e}")
+
+
+    def csv_dict_parser_str(self, in_file: str) -> dict:
+
+        """
+        Parses csv-file to dict containing str variable(s)\n
+        \n
+        ------------
+        PARAMETERS\n
+        in_file:\n
+        Takes input file name of type STRING\n
+        File must be of type CSV\n
+        \n
+        ------------
+        RETURNS\n
+        Returns a dict of values\n
+        Return is of type DICT\n
+        List elements is of type STR\n
+        \n
+        """
+
+        return_file = {}
+
+        #OPEN FILE AND CREATE READER OBJ
+        reader = []
+        delim = ','
+
+        try:
+            with open(in_file,'r') as file:
+                for line in file:
+                    reader.append(line.rstrip('\n').rstrip('\r').split(delim))
+                
+                self.log.debug(f"opened {in_file}")
+                #PARSE DATA FROM CSV TO DICT
+                for line in reader:
+                    add_data = {line[0] : line[1]}
+                    return_file.update(add_data)
+            return return_file
+        except Exception as e:
+            self.log.error (f"An error occured while parsing CSV: {e}")
---
+++ src/sim_env_esp_32/validator_csv.py
@@ -0,0 +1,69 @@
+# Version 1.00 | Encoding UFT-8
+# Created by: Jesper Hammer
+# Date: 05-05-2024
+
+class ValidatorCSV:
+    """
+    Validates content of csv file against corresponding schema file\n
+    List of class methods:\n
+    - Validate: validates content of csv file against corresponding schema file\n
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def validate(file, sch_file) -> bool:
+        """
+        Validates content of csv file against corresponding schema file\n
+        \n
+        ------------
+        PARAMETERS\n
+        file:\n
+        Takes name of input file of type STRING\n
+        File must be of type CSV\n
+        sch_file:\n
+        Takes name of schema file of type STRING\n
+        File must be of type CSV\n
+        \n
+        ------------
+        RETURNS\n
+        Returns result of validation\n
+        Return type is BOOL\n
+        \n
+        """
+
+        reader = []
+        sch = []
+        delim = ','
+        return_val = bool(False)
+
+        with open(file,'r') as file:
+            for line in file:
+                reader.append(line.rstrip('\n').rstrip('\r').split(delim))
+
+        with open(sch_file,'r') as file:
+            for line in file:
+                sch.append(line.rstrip('\n').rstrip('\r').split(delim))
+
+        #Generates list of validation rules based on schema
+        for i in range(len(sch[0])):
+            if str(sch[0][i]) == "str":
+                sch[0][i] = str("str")
+            if str(sch[0][i]) == "int":
+                sch[0][i] = int(1)
+
+        #Validates csv against validation rules
+        for i in range(len(reader)):
+            for j in range(len(reader[i])):
+                try:
+                    reader[i][j] == int(reader[i][j])
+                except ValueError:
+                    pass
+                
+                if type(reader[i][j]) == type(sch[0][j]):
+                    return_val = bool(True)
+                elif type(reader[i][j]) != type(sch[0][j]):
+                    return_val = bool(False)
+                    break
+        return return_val

8efa66004c23d06e5464f076848537292da55f35
Ib Leminen Mohr Nielsen <144134079+ibxx0402@users.noreply.github.com>
2024-05-13 14:42:08 +0200

    Optimised for esp32 client
    Removed functionality of reading xml files and can only read xml strings

---
+++ src/xml_parser/esp32_version/packet_controller_esp32.py
@@ -0,0 +1,16 @@
+#Version 1.00 | Encoding UTF-8
+#Created 22-04-2024
+#Created by: Ib Leminen Mohr Nielsen
+#Modified by: Frederik B. B. Jepsen, Ib Leminen Mohr Nielsen
+#Last modified 13-05-2024
+
+from lxml import etree
+        
+def validate(xsd_path: str, xml_input: str) -> bool:
+    xmlschema_doc = etree.parse(xsd_path)
+    xmlschema = etree.XMLSchema(xmlschema_doc)
+    
+    xml_doc= etree.fromstring(xml_input)
+    result = xmlschema.validate(xml_doc)
+
+    return result

0df663a0405e041b54b10cfa71d961d931d3a994
fjepse22 <144028671+fjepse22@users.noreply.github.com>
2024-05-13 08:51:48 +0200

    Updated log funktion
    Updated the log, so connecting to the sql-database is now a debug log and the log when data is inserted now also shows boat_ID and time for the log data.

--- src/xml_parser/sql_insert_data.py
+++ src/xml_parser/sql_insert_data.py
@@ -1,8 +1,8 @@
 # See https://mariadb-corporation.github.io/mariadb-connector-python/usage.html for documentation about the mariadb module.
-# Version 1.24
+# Version 1.25
 # Writen by Frederik B. B. Jepsen
 # Created 13-04-2024
-# last modified: 11-05-2024
+# last modified: 13-05-2024
 # modified by: Frederik Jepsen
 
 # TESTER PULL REQUEST
@@ -59,7 +59,7 @@ class DatabaseConnection:
 
         try:
             connection = mariadb.connect(user = self.user, password = self.password,host = self.host,port = self.port, database = self.database)
-            self.logger.info(f'Connected sucessfully to {self.database} with user {self.user}')
+            self.logger.debug(f'Connected sucessfully to {self.database} with user {self.user}')
         except mariadb.Error as e:
             self.logger.error(f'Error connecting to MariaDB Platform: {e}')
         # Get Cursor
@@ -154,16 +154,16 @@ class DatabaseConnection:
         # free resources
         cursor.close()
         connection.close()
-        self.logger.info(f'Data from xml-file inserted to database with Data_ID: {Data_ID}')
+        self.logger.info(f'Data from xml-file inserted to database with Data_ID: {Data_ID}, Boat_ID: {boat_ID} log_time: {date}')
 
 if __name__ == '__main__':
 
-    ud= {"id" : 1, "pos_lat" : 3300000, "pos_lon" : -10400000, "time" : "14:12:10", "p_draw" : 0}
+    ud= {"id" : 1, "pos_lat" : 3300000, "pos_lon" : -10400000, "time" : "14:12:11", "p_draw" : 0}
     vd = {"batt_1" : 0, "batt_2" : 0, "batt_3" : 0, "batt_4": 0, "batt_5": 0, "batt_6": 0, "batt_7" : 0, "batt_8" : 0}
     td = {"batt_1" : 0, "batt_2" : 0, "batt_3" : 0, "batt_4": 0, "batt_5": 0, "batt_6": 0, "batt_7" : 0, "batt_8" : 0}
 
     # Testserver on Frederik's own computer.
-    Goboat = DatabaseConnection(user="frederik",password="password",host="127.0.0.1",database='goboatv2',directory="C:/Users/frede/Downloads",log_level = logging.DEBUG)
+    Goboat = DatabaseConnection(user="frederik",password="password",host="127.0.0.1",database='goboatv2',directory="C:/Users/frede/Downloads",log_level = logging.INFO)
     data = xml_p.XmlParser(directory="C:/Users/frede/Downloads", unit_dict=ud, voltage_dict=vd, temp_dict=td)
     data.get_all_data()
 

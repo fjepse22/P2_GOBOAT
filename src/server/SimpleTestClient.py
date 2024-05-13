8f8ce4382e692b74335b3e5cc006019b3fb7920d
Ib Leminen Mohr Nielsen <144134079+ibxx0402@users.noreply.github.com>
2024-05-13 10:34:47 +0200

    Added dict support

--- src/server/SimpleTestClient.py
+++ src/server/SimpleTestClient.py
@@ -1,26 +1,49 @@
 """"test client, used together with TCPSERVER.py to test the server"""
-
 from socket import *
+import json
 
-SERVER_IP = "192.168.1.10"
-SERVER_PORT = 8888
-BUFFER_SIZE = 1024
+class TCPClient:
+    def __init__(self):
+        pass
+
+    def send_data(self, unit_dict, voltage_dict, temp_dict):
+        """
+        Sends data to the server.
+        \n
+        ------------
+        PARAMETERS\n
+        \n
+        data_unit_dict: Dictionary with the data from the unit.
+        data_batt_voltage_dict: Dictionary with the battery voltage data.
+        data_batt_temp_dict: Dictionary with the battery temperature data.
+        \n
+        ------------
+        RETURNS\n
+        \n
+        Returns "None"\n
+        Return type is None.\n
+        """
+      
+        SERVER_IP = "192.168.1.140"
+        SERVER_PORT = 8888
+        BUFFER_SIZE = 1024
 
 
-s = socket(AF_INET,SOCK_STREAM)
-s.connect((SERVER_IP, SERVER_PORT))
+        s = socket(AF_INET,SOCK_STREAM)
+        s.connect((SERVER_IP, SERVER_PORT))
+
+        s.send(json.dumps(unit_dict).encode('utf-8')+b'#')
+        s.send(json.dumps(voltage_dict).encode('utf-8')+b'#')
+        s.send(json.dumps(temp_dict).encode('utf-8')+b'#')
+        s.send(b'EOF')
+
+        s.close()
 
 
-file = open("status_data.xml", "rb")
-encoded_string = file.read(BUFFER_SIZE)
-
-while encoded_string:
-    s.send(encoded_string)
-    encoded_string = file.read(BUFFER_SIZE)
-
-file.close()
-s.send(b'EOF')
-s.close()
-print("Data sent.")
-
-
+if __name__ == "__main__":
+    unit_dict = {"id" : 1, "pos_lat" : 3300000, "pos_lon" : -10400000, "time" : "14:12:10", "p_draw" : 0}
+    voltage_dict = {"batt_1" : 0, "batt_2" : 0, "batt_3" : 0, "batt_4": 0, "batt_5": 0, "batt_6": 0, "batt_7" : 0, "batt_8" : 0}
+    temp_dict = {"batt_1" : 0, "batt_2" : 0, "batt_3" : 0, "batt_4": 0, "batt_5": 0, "batt_6": 0, "batt_7" : 0, "batt_8" : 0}
+    
+    client = TCPClient()
+    client.send_data(unit_dict, voltage_dict, temp_dict)

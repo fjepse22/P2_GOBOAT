8c67346c2c976f520f85309bcc8a01bb50fff6ca
Ib Leminen Mohr Nielsen <144134079+ibxx0402@users.noreply.github.com>
2024-05-17 08:52:09 +0200

    made new receiving method for more secure headers
    # Renamed some methods
    # changed self.conn to conn, as it caused problems when receiving messages very fast. 
    # Made a dedicated receive function, ensuring the header is more protected from potential errors. 
    # Stops receiving data, faster if header is misleading.

--- src/server/server_esp32/TCPServer_esp32.py
+++ src/server/server_esp32/TCPServer_esp32.py
@@ -2,7 +2,7 @@
 #Created 23-04-2024
 #Created by: Ib Leminen Mohr Nielsen
 #Modified by: Frederik B. B. Jepsen, Ib Leminen Mohr Nielsen
-#Last modified 14-05-2024
+#Last modified 15-05-2024
 
 import socket
 import selectors
@@ -11,10 +11,6 @@ import time
 from logger import log
 import sql_insert_data as sql
 from xml_parser_esp32 import XmlParser
-
-"""
-This needs to be event based with a que system, in order to ensure that all data gets into the database
-"""
 
 class SQL_socket:
     """
@@ -26,16 +22,16 @@ class SQL_socket:
     List of class methods:\n
     - __init__(self,HOST='',PORT=8888,CONN_COUNTER=0,BUFFER_SIZE=1024,user= None,password= None,host = None): Initialises the object and starts the process to listen\n
 
-    - __accept(self,sock, mask): Used to establish a connection to a client, that sends a xml-file.\n
+    - __accept_connection(self,sock, mask): Used to establish a connection to a client, that sends a xml-file.\n
 
     - __read(self, sock, mask): This method is used to recieve the xml-data from a client and insert it into the database\n
 
-    - __insert_data(self,xml):  This method is used to connect to the Goboat database and insert the data from the xml-file, should only be called by the __read() method. \n
+    - __insert_data(self,xml):  This method is used to connect to the Goboat database and insert the data from the xml-file, should only be called by the __read_data() method. \n
 
     - run(self): This method calls the loop that will constantly listen for something on the specified port self.PORT\n
     """
 
-    def __init__(self,HOST='',PORT=8888,CONN_COUNTER=0,BUFFER_SIZE=1024,user= None,password= None,host = None, directory="/home/Gruppe250/test",database='goboatv2') -> None:
+    def __init__(self,HOST='',PORT=8888,CONN_COUNTER=0,BUFFER_SIZE=1024,user= None,password= None,host = None, directory="/home/Gruppe250/Server_ESP32",database='goboatv2') -> None:
         """
          Initialises the object and starts the process to listen
         """
@@ -57,11 +53,11 @@ class SQL_socket:
         self.sock.bind((self.HOST, self.PORT))
         self.sock.listen(100)
         self.sock.setblocking(False)
-        self.sel.register(self.sock, selectors.EVENT_READ, self.__accept)
+        self.sel.register(self.sock, selectors.EVENT_READ, self.__accept_connection)
         
         #This start the the listening process
 
-    def __accept(self,sock, mask):
+    def __accept_connection(self,sock, mask):
         """
         This private method is used to establish a connection to a client, that sends a xml-file, Should not be run by a user.\n
         \n
@@ -77,19 +73,18 @@ class SQL_socket:
         Returns "None"\n
         Return None\n
         """
-
-        self.conn, self.addr = self.sock.accept() # Should be ready
-        self.conn.setblocking(False)
-        self.sel.register(self.conn, selectors.EVENT_READ, self.__read)
-
-
-    def __read(self,sock, mask): 
+        conn, addr = self.sock.accept() # Should be ready
+        conn.setblocking(False)
+        self.sel.register(conn, selectors.EVENT_READ, self.__read_data)
+    
+    
+    def __read_data(self, conn, mask): 
         """
-        This private method is used to recieve the xml-data from a client and insert it into the SQL-database\n
+        This private method is used to read, recieve and store data\n
         \n
         ------------
         PARAMETERS\n
-        sock:\n
+        conn:\n
         mask:\n
     
         self:\n
@@ -99,37 +94,71 @@ class SQL_socket:
         Returns "None"\n
         Return None\n
         """
-        
-        data=b''
-        header = self.conn.recv(3)
+        self.CONN_COUNTER += 1
+        log.debug(f"Connection number {self.CONN_COUNTER} has been established")
+        self.start_time = time.time()
+
+        # Recieves the header max 3 bytes ie ((2^24)-1) length of data
+        header=self.__recieved_data(conn, 3)
+        if header is None:
+            self.sel.unregister(conn)
+            conn.close()
+            log.debug(f"Connection number {self.CONN_COUNTER} has been closed")
+            return
         log.debug(f"header length {int.from_bytes(header, 'big')}")
         
-        start_time = time.time()
-        while len(data) < int.from_bytes(header, 'big'):
-            try:
-                data += self.conn.recv(int.from_bytes(header, 'big'))
-            except BlockingIOError:
-                continue
-            
-            if time.time()-start_time > 1:
-                log.error("Timeout as data is not recieved in 1 second.")
-                break
-          
-        log.debug(f"Data length {len(data)}")
+        # Recieves the data, using the length of the data as buffer size
+        data=self.__recieved_data(conn, int.from_bytes(header, 'big'))
+        if data is None:
+            self.sel.unregister(conn)
+            conn.close()
+            log.debug(f"Connection number {self.CONN_COUNTER} has been closed")
+            return
+        log.debug(f"data length {len(data)}")
 
-        self.sel.unregister(self.conn)
-        self.conn.close()
-
+        # Creates a XML-file out of the data.
         self.__insert_data(data)
 
-        self.CONN_COUNTER +=1
-
-
-
+        # Unregisters the socket and closes the connection
+        self.sel.unregister(conn)
+        conn.close()
+        log.debug(f"Connection number {self.CONN_COUNTER} has been closed")
+        
+    def __recieved_data(self,conn, data_size, timeout=1):
+        """
+        This private method is used to reacieve data\n
+        \n
+        ------------
+        PARAMETERS\n
+        conn: Used for connecting to client.\n
+        data_size: Legth of incomming message.\n
+        timeout: Specified timeout for while loop if it doesnt close.\n
+    
+        self:\n
+        ------------
+        RETURNS\n
+        \n
+        Returns "data": The incomming data from a client\n
+        Return type Bytes\n
+        """
+        data = b''
+        while len(data) < data_size:
+            try:
+                current_data = conn.recv(data_size-len(data))
+                if not current_data:
+                    log.error(f"Connection closed by client")
+                    return None
+                data += current_data
+            except BlockingIOError:
+                continue
+            if time.time() - self.start_time >= timeout:
+                log.error(f"Timeout as data is not recieved in {timeout}s.")
+                return None
+        return data
 
     def __insert_data(self,data):
         """
-        This private method is used to connect to the Goboat database and insert the data from the xml-file, should only be called by the __read() method.\n
+        This private method is used to connect to the Goboat database and insert the data from the xml-file, should only be called by the __read_data() method.\n
         \n
         ------------
         PARAMETERS\n
@@ -143,36 +172,26 @@ class SQL_socket:
         Return None\n
         """
         log.info("processing data.....")
-        safe_data = True
         try:
             data=data.split(b'#')
-        except TypeError:
-            safe_data=False
-            log.error(f"""File: TCPSERVER.py ErrorType: TypeError: The type is {type(data)}""")
+            unit_dict=json.loads(data[0].decode('utf-8'))
+            voltage_dict=json.loads(data[1].decode('utf-8'))
+            temp_dict=json.loads(data[2].decode('utf-8'))  
         
-        if safe_data==True:
-            log.debug(f"Data has been split")
-
-            try:
-                unit_dict=json.loads(data[0].decode('utf-8'))
-                voltage_dict=json.loads(data[1].decode('utf-8'))
-                temp_dict=json.loads(data[2].decode('utf-8'))
-                
-            except json.decoder.JSONDecodeError:
-                safe_data=False
-                log.error(f"""File: TCPSERVER.py ErrorType: json.decoder.JSONDecodeError Couldn't convert bytes into dictionary""")
+        except (json.decoder.JSONDecodeError, IndexError, TypeError, UnicodeDecodeError) as e:
+                log.error(f"""File: TCPSERVER.py: {e}""")
+                return
     
-        if safe_data==True:
-            log.debug(f"Data is being converted into dictionaries")
-            xml_parser= XmlParser(xsd_path=(self.directory+"/sch_status_data.xsd"), directory=self.directory, unit_dict = unit_dict, voltage_dict=voltage_dict, temp_dict=temp_dict) #inserets the xml data into the xml_parser from the client.
-            xml_parser.get_all_data()
+        log.debug(f"Data is being converted into original input")
+        xml_parser= XmlParser(xsd_path=(self.directory+"/sch_status_data.xsd"), directory=self.directory, unit_dict = unit_dict, voltage_dict=voltage_dict, temp_dict=temp_dict) #inserets the xml data into the xml_parser from the client.
+        xml_parser.get_all_data()
 
-            if xml_parser.valid_xml == True:
-                log.info(f"XML is valid")
-                Goboat = sql.DatabaseConnection(user=self.user,password=self.password,host=self.host, port=3306,database=self.database, directory=self.directory) # establish connection to the database
-                input = xml_parser
+        if xml_parser.valid_xml == True:
+            log.info(f"XML is valid")
+            Goboat = sql.DatabaseConnection(user=self.user,password=self.password,host=self.host, port=3306,database=self.database, directory=self.directory) # establish connection to the database
+            input = xml_parser
 
-                Goboat.insert_boat_data(boat_ID=input.boat_id,date=input.date,lok_lat=input.lok_lat,lok_long=input.lok_long,temperatures=input.temp_list,watt=input.watt,voltage_array=input.voltage_list)
+            Goboat.insert_boat_data(boat_ID=input.boat_id,date=input.date,lok_lat=input.lok_lat,lok_long=input.lok_long,temperatures=input.temp_list,watt=input.watt,voltage_array=input.voltage_list)
 
 
     def run(self):
